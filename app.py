62a30100909944af5830a205daf99a892ff7ca39
Gayathri Jayan <147328107+Gayathri-Jayan@users.noreply.github.com>
2024-11-17 02:25:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,10 @@ def smile_percentage_route():
     global smile_percentage
     return jsonify(smile_percentage=smile_percentage)
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable, default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
